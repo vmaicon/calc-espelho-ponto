8c5bd242aee227482934cdbe494f9b199d7b698f
Maicon Vasconcelos de Araújo <maicon.araujo@tjac.jus.br>
2025-04-01 13:11:32 -0500

    altera posição do botão

--- calculadora-de-horas.js
+++ calculadora-de-horas.js
@@ -1,6 +1,6 @@
 let total_horas = new Array();
 let dias_trabalhado = 0;
-let dias_folgas =[
+let dias_folgas = [
     { data: "06/03/2025", descricao: "50 - FOLGA RECESSO FORENSE (GOZO)", tipo: "pesssoal" },
     { data: "07/03/2025", descricao: "50 - FOLGA RECESSO FORENSE (GOZO)", tipo: "pesssoal" },
 ];
@@ -155,35 +155,26 @@ function calcularSaldoHoras(dates, horasDeTrabalho = 7) {
 
 // Função para criar e posicionar o botão
 function criarBotaoNoTopo() {
-    // Cria o botão
     const botao = document.createElement("button");
-
-    // Define o texto do botão
     botao.innerText = "Calcula Horas";
     botao.classList.add("ui-button", "ui-widget", "ui-state-default", "ui-corner-all", "ui-button-text-icon-left", "btn-main-action");
-
-    // Estiliza o botão para ficar fixo no topo e à frente
+    
+    // Remove posicionamento fixo e ajusta estilo para combinar com outros botões
     Object.assign(botao.style, {
-        position: "fixed",  // Fixa o botão em relação à janela
-        top: "70px",        // Distância do topo
-        right: "10px",      // Distância da direita
-        zIndex: "10000",    // Certifica-se de que está acima de outros elementos
-        padding: "10px 20px", // Estilização do botão
-        fontSize: "16px",     // Tamanho da fonte
-        backgroundColor: "#007BFF", // Cor de fundo
-        color: "#FFF",          // Cor do texto
-        border: "none",         // Sem borda
-        borderRadius: "5px",    // Bordas arredondadas
-        cursor: "pointer",      // Cursor como mãozinha
+        marginLeft: "10px",
+        backgroundColor: "#007BFF",
+        color: "#FFF"
     });
 
-    // Adiciona um evento ao botão
     botao.addEventListener("click", () => {
         atualizarTotais();
     });
 
-    // Adiciona o botão ao corpo do documento
-    document.body.appendChild(botao);
+    // Encontra o container alvo
+    const targetDiv = document.querySelector('.ui-fieldset-content .row .col-lg-5.col-sm-4.print-button');
+    if (targetDiv) {
+        targetDiv.appendChild(botao);
+    }
 }
 
 function atualizarTotais() {
